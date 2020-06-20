1464804fc5a80b62e3bf0b59841240d28ce80b2c
kit-lec <kit_lec@hotmail.com>
2020-06-20 18:47:50 +0900

    200620-2

---
+++ 문서및자료/test_col01.sql
@@ -0,0 +1,73 @@
+SET LINESIZE 120;
+SET PAGESIZE 100;
+
+-- t_student t_professor t_department t_exam01 t_credit 용
+COL studno FOR 9999; 
+COL name FOR a8;	
+COL id FOR a10;		
+COL grade FOR 9;	
+COL jumin FOR a13;      
+COL tel FOR a14          
+COL height FOR 999;	
+COL weight FOR 99;	
+COL deptno1 FOR 999;	
+COL deptno2 FOR 999;	
+COL profno FOR 9999;
+COL position FOR a8;
+COL pay FOR 999999999;
+COL bonus FOR 999;
+COL deptno FOR 999;
+COL email FOR a20;
+COL hpage FOR a25;
+COL dname FOR a16;
+COL part FOR 999;
+COL build FOR a12;
+
+
+-- t_emp, t_dept 용
+COL empno for 99999999;
+COL ename for a6;
+COL job for a9;
+COL mgr for 9999;
+COL sal for 99999;
+COL comm for 9999;
+COL detpno for 999;
+COL loc FOR a10;
+
+-- t_emp2, t_dept2, t_post 용
+COL emp_type FOR a8;
+COL tel FOR a14;
+COL hobby FOR a10;
+COL post FOR a8;
+COL pempno FOR 99999999;
+COL s_age FOR 999;
+COL e_age FOR 999;
+COL s_year FOR 999;
+COL e_year FOR 999;
+
+-- t_customer, t_gift 용
+COL c_name FOR a8;
+COL c_jumin FOR a13;
+COL g_no FOR 9999;
+COL g_name FOR a14;
+
+-- t_sales, t_product 용
+COL s_date FOR a8;
+COL s_code FOR 9999;
+COL s_qty FOR 999;
+COL s_total FOR 99999;
+COL s_store FOR a5;
+COL p_code FOR 9999;
+COL p_name FOR a8;
+COL p_price FOR 99999;
+
+-- t_member 용
+COL m_no FOR 9999;
+COL m_name FOR a10;
+COL m_jumin FOR a13;
+COL m_passwd FOR a10;
+COL m_id FOR a10;
+COL m_question FOR a10;
+COL m_answer FOR a10;
+
+
